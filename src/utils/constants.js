5927929f514d1001f0255befbff5569bebae3f53
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-12-14 22:53:10 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 const apiKey = process.env.REACT_APP_API_KEY;
 export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=${apiKey}&maxResults=50`;
 export const YOUTUBE_SUGGESTIONS_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
